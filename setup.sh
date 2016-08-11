0e0d88b11e2bbc18b9a58d65952dbd1566450a44
Xiaolong Ye <xiaolong.ye@intel.com>
2016-08-11 15:07:23 +0800

    Introduce setup.sh
    It's used to create symbols for all dotfiles, mostly need to run it when
    setup my environment in a new machine.
    
    Note:
    Some prerequisites are required:
    1. spf13 vim has been installed (may need to install vim from src)
    2. youcompleteme has been installed and configured
    3. mutt has been installed and configured (including
       msmtp/getmail/procmail configuration, mailbox setup)
    
    Signed-off-by: Xiaolong Ye <xiaolong.ye@intel.com>

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "!! CAUTIONS: It will recreate all the symbols !!"
+echo "Are you sure? [y/n]"
+
+while true; do
+	read input
+	case $input in
+		[Yy]* ) break;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
+
+DOTFILE_PATH=$(pwd)
+echo "\ndotfile repository path: $DOTFILE_PATH"
+
+# create symbols for vim
+ln -sf $DOTFILE_PATH/vimrc.before.local ~/.vimrc.before.local
+ln -sf $DOTFILE_PATH/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf $DOTFILE_PATH/vimrc.local ~/.vimrc.local
+
+# create symbols for mutt
+ln -sf $DOTFILE_PATH/muttrc ~/.mutt/muttrc
+
+echo "\nDone"
