daf62b6dd5ed45ed1756590873154fca973a601f
Xiaolong Ye <xiaolong.ye@intel.com>
2018-10-28 14:51:24 +0800

    spacemacs: put imenu list in the left
    Signed-off-by: Xiaolong Ye <xiaolong.ye@intel.com>

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -31,6 +31,8 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     (imenu-list :variables
+                 imenu-list-position 'left)
      html
      python
      go
