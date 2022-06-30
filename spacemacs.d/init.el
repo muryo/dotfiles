0825239d4b8a5bfb40c9111f7fc9c95133e33e7a
Xiaolong Ye <xiaolongye@tencent.com>
2022-06-30 14:29:56 +0800

    spacemacs: support auto-completion for python
    also need to:
    1. pip install jedi
    2. edit ./elpa/anaconda-mode-20211122.817/anaconda-mode.py
       remove '--always-copy' in install_deps_setuptools.
    
    Signed-off-by: Xiaolong Ye <xiaolongye@tencent.com>

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -31,12 +31,12 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
-	 lua
+     (python :variables python-backend 'anaconda)
+     lua
      graphviz
      (imenu-list :variables
                  imenu-list-position 'left)
      html
-     python
      go
      c-c++
      ;; ((c-or-c++-mode :variables
@@ -57,8 +57,8 @@ values."
      ;;(markdown :variables markdown-live-preview-engine 'vmd)
      markdown
      (git :variables
-	  git-commit-fill-column 75
-	  git-commit-summary-max-length 60)
+          git-commit-fill-column 75
+          git-commit-summary-max-length 60)
      ;; (version-control :variables
      ;;                  version-control-diff-tool 'diff-hl
      ;;                  version-control-diff-side 'left
@@ -78,6 +78,7 @@ values."
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '(dtrt-indent)
+   dotspacemacs-additional-packages '(jedi)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -362,7 +363,7 @@ you should place your code here."
   (define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
   (define-key evil-normal-state-map (kbd "M-.") 'helm-gtags-dwim)
   (define-key evil-normal-state-map (kbd "M-,") 'helm-gtags-pop-stack)
-  (setq python-shell-interpreter "python3")
+  (setq python-shell-interpreter "python2")
   (setq create-lockfiles nil)
   (setq xterm-query-timeout nil)
   (spaceline-define-segment buffer-id
@@ -404,7 +405,7 @@ you should place your code here."
  '(magit-commit-arguments (quote ("--signoff")))
  '(package-selected-packages
    (quote
-    (lua-mode evil-magit magit-gitflow magit-popup magit magit-section transient compat nadvice evil-collection package-safe-delete molikai-theme solarized-theme molaikai-theme fringe-helper git-gutter+ undo-tree web-completion-data git-gutter org-category-capture powerline pcre2el alert log4e gntp org-plus-contrib markdown-mode hydra lv dash-functional parent-mode projectile request xcscope haml-mode gitignore-mode flyspell-correct pos-tip flycheck pkg-info epl flx highlight smartparens iedit anzu evil goto-chg go-mode inf-ruby bind-key yasnippet packed anaconda-mode pythonic avy auto-complete company f git-commit with-editor helm helm-core popup async dash s graphviz-dot-mode imenu-list yapfify yaml-mode xterm-color ws-butler winum which-key web-mode volatile-highlights vi-tilde-fringe uuidgen use-package toc-org tagedit stickyfunc-enhance srefactor spaceline smeargle slim-mode shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe restart-emacs rbenv ranger rake rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode popwin pip-requirements persp-mode paradox orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets open-junk-file neotree multi-term move-text monokai-theme mmm-mode minitest markdown-toc macrostep lorem-ipsum live-py-mode linum-relative link-hint less-css-mode indent-guide hy-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-flx helm-descbinds helm-css-scss helm-cscope helm-company helm-c-yasnippet helm-ag google-translate golden-ratio go-guru go-eldoc gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md ggtags fuzzy flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav dumb-jump dtrt-indent disaster diminish diff-hl define-word cython-mode company-web company-statistics company-go company-c-headers company-anaconda column-enforce-mode cmake-mode clean-aindent-mode clang-format chruby bundler auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
+    (jedi jedi-core python-environment epc ctable concurrent deferred lua-mode evil-magit magit-gitflow magit-popup magit magit-section transient compat nadvice evil-collection package-safe-delete molikai-theme solarized-theme molaikai-theme fringe-helper git-gutter+ undo-tree web-completion-data git-gutter org-category-capture powerline pcre2el alert log4e gntp org-plus-contrib markdown-mode hydra lv dash-functional parent-mode projectile request xcscope haml-mode gitignore-mode flyspell-correct pos-tip flycheck pkg-info epl flx highlight smartparens iedit anzu evil goto-chg go-mode inf-ruby bind-key yasnippet packed anaconda-mode pythonic avy auto-complete company f git-commit with-editor helm helm-core popup async dash s graphviz-dot-mode imenu-list yapfify yaml-mode xterm-color ws-butler winum which-key web-mode volatile-highlights vi-tilde-fringe uuidgen use-package toc-org tagedit stickyfunc-enhance srefactor spaceline smeargle slim-mode shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe restart-emacs rbenv ranger rake rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode popwin pip-requirements persp-mode paradox orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets open-junk-file neotree multi-term move-text monokai-theme mmm-mode minitest markdown-toc macrostep lorem-ipsum live-py-mode linum-relative link-hint less-css-mode indent-guide hy-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-flx helm-descbinds helm-css-scss helm-cscope helm-company helm-c-yasnippet helm-ag google-translate golden-ratio go-guru go-eldoc gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md ggtags fuzzy flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav dumb-jump dtrt-indent disaster diminish diff-hl define-word cython-mode company-web company-statistics company-go company-c-headers company-anaconda column-enforce-mode cmake-mode clean-aindent-mode clang-format chruby bundler auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
